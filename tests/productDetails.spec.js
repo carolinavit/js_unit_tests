bdb00f01d00e943be9b10a18d563c1e8f1fa5080
Vitória <vitcs11@gmail.com>
2022-09-21 20:38:56 -0300

    finalmente requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,13 +31,21 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    expect(typeof productDetails).toBe('function');
     // Teste se o retorno da função é um array.
+    expect(Array.isArray(productDetails())).toBe(true);
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productDetails('Alcool', 'Máscara').length).toBe(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+     expect(typeof Object.values(productDetails('Alcool', 'Máscara'))).toBe('object');
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(productDetails('Alcool', 'Máscara')).not.toEqual([{name: 'Alcool', details:{productId: 'Alcool123'}},{name: 'Alcool', details:{productId: 'Alcool123'}}])
     // Teste se os dois productIds terminam com 123.
+    const param = productDetails('Alcool', 'Máscara');
+    expect(param[0]['details'].productId).toMatch('123');
+    expect(param[1]['details'].productId).toMatch('123');
   });
 });
