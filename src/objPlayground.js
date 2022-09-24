0ce431145a922cad03d1ce3914b0c5c084d454fd
Vitória <vitcs11@gmail.com>
2022-09-24 12:34:57 -0300

    Requisito 8

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -35,13 +35,13 @@
 */
 
 const calculator = (number1, number2) => {
-  let obj = {
+  let operation = {
     sum: number1 + number2,
     mult: number1 * number2,
     div: Math.floor(number1 / number2),
     sub: number1 - number2,
   };
-  return obj;
+  return operation;
 };
 
 const arrayGenerator = (type, object) => {
